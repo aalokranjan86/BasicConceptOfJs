e05d9f7328bc6f4bb7fc7ab799d64a548dd812fc
Aalok Ranjan <aalokranjan8629@gmail.com>
2020-07-15 11:46:05 +0530

    Added another file web1

---
+++ web1/login.js
@@ -0,0 +1,15 @@
+// // Get the modal
+// var con = document.getElementById('id01');
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == con) {
+//     con.style.display = "none";
+//   }
+// }
+var con = document.getElementById('city');
+window.onclick = function(event){
+if (event.target === con) {
+  con.style.display='none'
+}
+}
